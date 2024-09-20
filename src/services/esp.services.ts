23671eeea74fd52db15072b586ac71637bb45171
MuryloSaladino <murylosaladino@gmail.com>
2024-09-20 00:40:47 -0300

    read collar service

--- src/services/esp.services.ts
+++ src/services/esp.services.ts
@@ -1,7 +1,13 @@
+import dayjs from "dayjs";
 import AppDataSource from "../data-source"
+import Pet from "../entities/Pet.entity";
 import PetDoor from "../entities/PetDoor.entity"
+import AppError from "../errors";
+import { createPetService } from "./pets.services";
+import DoorPermission from "../entities/DoorPermission.entity";
+import PermissionRange from "../entities/PermissionRange.entity";
 
-export const ensureEspRegister = async (mac: string) => {
+export const ensureEspRegister = async (mac:string) => {
 
     const repo = AppDataSource.getRepository(PetDoor);
 
@@ -10,4 +16,52 @@ export const ensureEspRegister = async (mac: string) => {
 
     const newDoor = repo.create({ mac });
     await repo.save(newDoor);
+}
+
+export const readPetTag = async (mac:string, hash:string): Promise<void> => {
+
+    const petRepo = AppDataSource.getRepository(Pet);
+    const doorRepo = AppDataSource.getRepository(PetDoor);
+    
+    const door = await doorRepo.findOneBy({ mac });
+    const pet = await petRepo.findOneBy({ id: hash })
+
+    if(!door) throw new AppError("Door not found.", 404);
+    if(!pet) {
+        if(!door.user) throw new AppError("Door doesn't have a owner yet.");
+        
+        const numberOfPets = await petRepo.countBy({ user: door!.user })
+        await createPetService(door.user!.id!, { name: `Pet ${numberOfPets}` })
+        
+        throw new AppError("New Pet created, access app for details.");
+    }
+
+    const permissionRepo = AppDataSource.getRepository(DoorPermission);
+
+    const permission = await permissionRepo.findOneBy({ pet: pet, petDoor: door });
+    if(!permission) throw new AppError("Pet does not have permission", 403);
+    
+    const rangeRepo = AppDataSource.getRepository(PermissionRange);
+    
+    const ranges = await rangeRepo.findBy({ doorPermission: permission });
+    if(ranges.length < 1) return;
+
+    const hours = new Date().getHours();
+    const minutes = new Date().getMinutes();
+    
+    for (let i = 0; i < ranges.length; i++) {
+        const range = ranges[i];
+        
+        if(
+            (   
+                range.begginingHour! < hours ||
+                (range.begginingHour! == hours && range.begginingMinute! <= minutes)
+            ) &&
+            (   
+                range.finishHour! > hours ||
+                (range.finishHour! == hours && range.finishMinute! >= minutes)
+            )
+        ) return;
+    }
+    throw new AppError("Invalid access", 403);
 }
