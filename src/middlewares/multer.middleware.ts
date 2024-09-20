2d7ccf083b1a294af9fa7ef3383882934a1c13f3
MuryloSaladino <murylosaladino@gmail.com>
2024-09-20 13:54:13 -0300

    multer middleware

---
+++ src/middlewares/multer.middleware.ts
@@ -0,0 +1,26 @@
+import multer from "multer";
+import { v4 as uuid } from "uuid";
+import AppError from "../errors";
+
+const storage = multer.diskStorage({
+    destination: (_req, _file, callback) => {
+        callback(null, 'uploads/');
+    },
+    filename: (_req, file, callback) => {
+        callback(null, `${uuid()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({
+    storage,
+    fileFilter: (_req, file, callback) => {
+        const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        if (allowedMimeTypes.includes(file.mimetype)) {
+            callback(null, true);
+        } else {
+            throw new AppError("Invalid image format");
+        }
+    }
+})
+
+export default upload
